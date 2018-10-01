18aebe5d12c7d1261c9b160d3bf2df5733c48c69
Alec Allain <allainal@eos22.cis.gvsu.edu>
2018-10-01 12:19:40 -0400

    Added basic code for the Options class

---
+++ options.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+/**
+ * This class handles the options 
+ * menu for the game
+ * 
+ * @author Alec Allain
+ * @author Nate Wichman
+ * @author Runquan Ye
+ * @author Jon DeWent
+ * @version 9/30/18 
+ */
+
+/** Global variables */
+
+
+/**
+ * Main method
+ */
+int main (int argc, char** argv) {
+
+}
