e28625fb84733cc007b79ba46f5cb7b213fd6232
Schroederdan <Schroeda@mail.gvsu.edu>
2018-10-05 14:38:09 -0400

    added Weapons.cpp and Weapons.hpp

--- Weapons.cpp
+++ Weapons.cpp
@@ -1,19 +1,50 @@
 #include <string>
+#include "Weapons.hpp"
 
-class Weapon {
+
+	Weapons::Weapons(){
+		range = 0;
+		damage = 0;
+		type = 'x'; // set default to x can be changed / removed
+	}
+
+	Weapons::Weapons(std::string name, int range, int damage, char type){
+		this->name = name;
+		this->range = range;
+		this->damage = damage;
+		this->type = type;
+	}	
+
+
+	std::string Weapons::getName(){
+		return this->name;
+	}
 	
-	public:
-		//constructor with no parameters
-		Weapon();
 
-		Weapon(std::string name, int range, int damage, char type);
-		//Accessors
-		std::string getName();
-		int getRange();
-		int getDamage();
-		//ranged or melee
-		char getType();
+	int Weapons::getRange(){
+		return this->range;
+	}
 
+	int Weapons::getDamage(){
+		return this->damage;
+	}
+	
+	char Weapons::getType(){
+		return this->type;
+	}
 
+	void Weapons::setRange(int range){
+		this->range = range;
+	}
 
+	void Weapons::setDamage(int damage){
+		this->damage = damage;
+	}
 
+	void Weapons::setType(char type){
+		this->type = type;
+	}
+
+	void Weapons::setName(std::string name){
+		this->name = name;
+	}
