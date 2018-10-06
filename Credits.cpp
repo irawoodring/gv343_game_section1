313f2a22c1430be7895032728343ad8b2d4c89be
Logan Karney <karneyl@eos17.cis.gvsu.edu>
2018-10-05 20:09:40 -0400

    Added RenderWindow functionality.

--- Credits.cpp
+++ Credits.cpp
@@ -1,3 +1,28 @@
 #include "Credits.h"
-#include <stdlib.h>
+#include <iostream>
 
+Credits::Credits(sf::RenderWindow& gameWindow){
+	this -> window = &gameWindow;
+}
+
+sf::RenderWindow* Credits::getWindow(){
+	return window;
+}
+
+int Credits::start(){
+	window -> clear();
+	window -> display();
+	std::cout << "press any char to continue" << std::endl;
+	char input;
+	std::cin >> input;
+
+	return 0;
+}
+
+
+
+/*int main(int argc, char** argv){
+	Credits c;
+	
+	return 0;
+}*/
--- Credits.h
+++ Credits.h
@@ -1,4 +1,20 @@
 #ifndef CREDITS_H_
 #define CREDITS_H_
 
+#include "SFML/Graphics.hpp"
+
+class Credits{
+	public:
+		Credits(sf::RenderWindow& gameWindow);
+		Credits();
+	
+	
+		int start();
+		sf::RenderWindow* getWindow();
+
+	private:
+		sf::RenderWindow* window;
+};
+
+
 #endif
