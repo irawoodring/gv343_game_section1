a52be4899d127666c020571077b742c50e300071
Wesley Harrison <harriswe@eos05.cis.gvsu.edu>
2018-10-07 16:44:25 -0400

    Added intro files.

---
+++ include/Intro.hpp
@@ -0,0 +1,20 @@
+#ifndef INTRO_H
+#define INTRO_H
+#include "SFML/Graphics.hpp"
+
+class Intro {
+
+	sf::RenderWindow window;
+
+    public:
+	Intro();
+	int passWindow( sf::RenderWindow& );
+	int showIntro();
+	// Add any private variables here. Including SFML members.
+    private:
+
+
+
+
+}
+#endif
