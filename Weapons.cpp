57cf6aaa7822a0c9722e6d0a79fe381942a20679
Schroederdan <Schroeda@mail.gvsu.edu>
2018-10-08 00:29:00 -0400

    can add a sprite to the board no collision detection see line 224 in game.cpp on how to draw

--- Weapons.cpp
+++ Weapons.cpp
@@ -1,7 +1,11 @@
 #include <string>
 #include "Weapons.hpp"
+#include "Monster.hpp"
+#include <iostream>
 
 	Weapons::Weapons(){
+		x = WIDTH/2;
+		y = HEIGHT/2;
 		range = 0;
 		damage = 0;
 		type = 'x'; // set default to x can be changed / removed
@@ -12,6 +16,8 @@
 		this->range = range;
 		this->damage = damage;
 		this->type = type;
+		x = WIDTH/2;
+		y = HEIGHT/2;
 	}	
 
 
@@ -47,3 +53,32 @@
 	void Weapons::setName(std::string name){
 		this->name = name;
 	}
+
+	void Weapons::setPosition(int x,int y){
+		if(x>=0 && x<=WIDTH)		
+			this->x = x;
+		if(y>=0 && y<=HEIGHT)		
+			this->y = y;
+		sprite.setPosition(x,y);
+	}
+
+	sf::Sprite Weapons::getSprite(){
+		return this->sprite;
+	}
+	
+	void Weapons::setSprite(std::string filename){
+		if(!texture.loadFromFile(filename)){
+			std::cerr << "Failed to load weapon texture: " << filename << "\n";
+			exit(EXIT_FAILURE);
+		}
+		sprite.setTexture(texture);
+		sf::FloatRect spriteSize = sprite.getGlobalBounds();
+		sprite.setOrigin(spriteSize.width/2.0,spriteSize.height/2.0);		
+	}
+
+	void Weapons::attack(Monster monster, int dmg){
+		monster.harm(dmg);
+	}
+
+
+
--- include/Weapons.hpp
+++ include/Weapons.hpp
@@ -3,6 +3,9 @@
 
 
 #include <string>
+#include "SFML/Graphics.hpp"
+#include "Settings.hpp"
+#include "Monster.hpp"
 
 class Weapons{
 	
@@ -26,13 +29,24 @@ class Weapons{
 		void setType(char type);
 		void setName(std::string name);
 
+		void setPosition(int x,int y);
+		sf::Sprite getSprite();
+		void setSprite(std::string filename);
+
+		void attack(Monster monster, int dmg);
 
 	private:
 		//all variables owned by a Weapon
 		int range;
 		int damage;
 		char type;
+
+		int x;
+		int y;
+
 		std::string name;
+		sf::Sprite sprite;
+		sf::Texture texture;
 
 
 };
