d9420ee50fd7810064846aac17419bf1a656000b
Emma <42856700+m4rkab@users.noreply.github.com>
2018-10-08 08:33:08 -0400

    Update Weapons.hpp

--- include/Weapons.hpp
+++ include/Weapons.hpp
@@ -1,39 +1,25 @@
-#ifndef  __H_WEAPONS__
-#define  __H_WEAPONS__
-
-
 #include <string>
-
-class Weapons{
+#include "SFML/Graphics.hpp"
+#include "Settings.hpp"
+#include "Monster.hpp"
+ class Weapons{
 	
-	public:
-		//constructor with no params
-		Weapons();
-
-		//Constructor with all params
-		Weapons(std::string name, int range, int damage, char type);
-		
-		//all getters
-		std::string getName();
-		int getRange();
-		int getDamage();
-		//ranged or melee
-		char getType();
-
-		//all setters
-		void setRange(int range);
-		void setDamage(int damage);
+class Weapons{
 		void setType(char type);
 		void setName(std::string name);
-
-
-	private:
+ 		void setPosition(int x,int y);
+		sf::Sprite getSprite();
+		void setSprite(std::string filename);
+ 		void attack(Monster monster, int dmg);
+	
+ 	private:
 		//all variables owned by a Weapon
 		int range;
 		int damage;
 		char type;
-		std::string name;
-
-
-};
-#endif
+ 		int x;
+		int y;
+ 		std::string name;
+		sf::Sprite sprite;
+		sf::Texture texture;
+ };
